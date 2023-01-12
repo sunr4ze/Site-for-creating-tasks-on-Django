38be718e46d590a05357785f7572a6fc82477080
sunr4zee <qwfqfqwqdqdq@gmail.com>
2023-01-12 14:34:18 +0300

    Работа с HTML-шаблонами и bootstrap

--- site_/site_app/urls.py
+++ site_/site_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('info/', views.info),
+    path('about/', views.about),
     path('contacts/', views.contacts)
 ]
--- site_/site_app/views.py
+++ site_/site_app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h4>Hello</h4>")
+    return render(request, 'site_app/index.html')
 
-def info(request):
-    return HttpResponse("<h1>О нас</h1>")
+def about(request):
+    return render(request, 'site_app/about.html')
 
 def contacts(request):
     return HttpResponse("<h1>Контакты</h1>")
