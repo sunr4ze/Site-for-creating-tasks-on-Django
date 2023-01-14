f8aef14e4942ac72b0a0c881b2c0630e5cf62758
sunr4zee <qwfqfqwqdqdq@gmail.com>
2023-01-14 22:01:36 +0300

    Вывод новых полей на страницу из БД

--- site_/site_app/models.py
+++ site_/site_app/models.py
@@ -6,7 +6,11 @@ class Task(models.Model):
     task = models.TextField('Описание')
     priority = models.CharField('Приоритет', max_length=40)
     deadline = models.CharField('Сроки выполнения', max_length=40)
+    status = models.CharField('Статус', max_length=40)
 
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
 
     def __str__(self):
         return self.title
--- site_/site_app/views.py
+++ site_/site_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Task
 
 def index(request):
-    return render(request, 'site_app/index.html')
+    tasks = Task.objects.all()
+    return render(request, 'site_app/index.html', {'title': 'Главная страница', 'tasks': tasks})
 
 def about(request):
     return render(request, 'site_app/about.html')
